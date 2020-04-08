5b2c84cd93bba541f3d3ab6aecd415b2690ebaff
Istvan Dumitras <d.steven@protonmail.ch>
2020-04-08 19:00:15 +0200

    basics

---
+++ thaz-management-backend/src/main/java/hu/tarsashazkezelo/tarsashaz/TarsashazApplication.java
@@ -0,0 +1,13 @@
+package hu.tarsashazkezelo.tarsashaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarsashazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarsashazApplication.class, args);
+	}
+
+}
