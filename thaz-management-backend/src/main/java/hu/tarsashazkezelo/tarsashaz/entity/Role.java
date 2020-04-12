d5881e4903890871d64b9d9c488a8f40bdd75086
Istvan Dumitras <d.steven@protonmail.ch>
2020-04-12 16:23:26 +0200

    user entity

---
+++ thaz-management-backend/src/main/java/hu/tarsashazkezelo/tarsashaz/entity/Role.java
@@ -0,0 +1,8 @@
+package hu.tarsashazkezelo.tarsashaz.entity;
+
+public enum Role {
+    OWNER,
+    TENANT,
+    RESIDENT,
+    ADMIN
+}
--- thaz-management-backend/src/main/java/hu/tarsashazkezelo/tarsashaz/entity/Users.java
+++ thaz-management-backend/src/main/java/hu/tarsashazkezelo/tarsashaz/entity/Users.java
@@ -1,4 +1,52 @@
 package hu.tarsashazkezelo.tarsashaz.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "Kozos_kepviselo_teljes_neve")
+    private String fullNameOfRepresentative;
+
+    @Column(name = "cegnev")
+    private String companyName;
+
+    @Column
+    private Integer taxNumber;
+
+    @Column //közös k. iroda címe
+    private String addressOfRepresentative;
+
+    @Column
+    private String ownerName;
+
+    @Column
+    private String ownerAddress;
+
+    @Column
+    private String email;
+
+    @Transient
+    private String password;
+
+    @Column
+    private Integer phoneNumber;
+
+    @Column
+    private Role role;
+
+    @Column // tulajdoni hányad pl 40-60%
+    private Integer ownership;
+
 }
