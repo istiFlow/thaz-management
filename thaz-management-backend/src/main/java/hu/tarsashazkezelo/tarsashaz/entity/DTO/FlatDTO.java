ba8e0a881b650ef6cb7830a7b1f5509855a720a5
Istvan Dumitras <d.steven@protonmail.ch>
2020-04-13 08:21:30 +0200

    .

--- thaz-management-backend/src/main/java/hu/tarsashazkezelo/tarsashaz/entity/DTO/FlatDTO.java
+++ thaz-management-backend/src/main/java/hu/tarsashazkezelo/tarsashaz/entity/DTO/FlatDTO.java
@@ -2,6 +2,7 @@ package hu.tarsashazkezelo.tarsashaz.entity.DTO;
 
 import hu.tarsashazkezelo.tarsashaz.entity.Flat;
 import hu.tarsashazkezelo.tarsashaz.entity.Role;
+import hu.tarsashazkezelo.tarsashaz.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,20 +25,23 @@ public class FlatDTO {
     private Integer flatSize;
     private Integer floorNumber;
     private Role nameOfActualUser;
-    private BuildingDTO building;
+    private BuildingDTO buildingDTO;
+    private OwnerDTO ownerDTO;
 
     public FlatDTO(Flat model){
         this.setId(model.getId());
         this.setFlatSize(model.getFlatSize());
         this.setFloorNumber(model.getFloorNumber());
         this.setNameOfActualUser(model.getNameOfActualUser());
-        this.setBuilding(new BuildingDTO(model.getBuilding()));
+        this.setBuildingDTO(new BuildingDTO(model.getBuilding()));
+        this.setOwnerDTO(new OwnerDTO(model.getUser()));
     }
 
     public Flat toEntity() {
         Flat flat = new Flat();
         BeanUtils.copyProperties(this,flat);
-        flat.setBuilding(this.getBuilding().toEntity());
+        flat.setBuilding(this.getBuildingDTO().toEntity());
+        flat.setUser(this.getOwnerDTO().toEntity());
         return flat;
     }
 }
--- thaz-management-backend/src/main/java/hu/tarsashazkezelo/tarsashaz/entity/DTO/OwnerDTO.java
+++ thaz-management-backend/src/main/java/hu/tarsashazkezelo/tarsashaz/entity/DTO/OwnerDTO.java
@@ -1,4 +1,42 @@
 package hu.tarsashazkezelo.tarsashaz.entity.DTO;
 
+import hu.tarsashazkezelo.tarsashaz.entity.Role;
+import hu.tarsashazkezelo.tarsashaz.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@Builder
 public class OwnerDTO {
+
+    private Long id;
+    private String address;
+    private String name;
+    private String email;
+    private String password;
+    private Integer phoneNumber;
+    private Role role;
+
+    public OwnerDTO(User user){
+        this.id = user.getId();
+        this.address = user.getAddress();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.phoneNumber = user.getPhoneNumber();
+        this.role = user.getRole();
+    }
+
+    public User toEntity(){
+        User user = new User();
+        BeanUtils.copyProperties(this,user);
+        return user;
+    }
 }
