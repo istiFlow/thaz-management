7dfddfc762ebe6473503671eebb3372be17c735f
Istvan Dumitras <d.steven@protonmail.ch>
2020-04-17 09:34:50 +0200

    dto's

--- thaz-management-backend/src/main/java/hu/tarsashazkezelo/tarsashaz/entity/DTO/ContractDTO.java
+++ thaz-management-backend/src/main/java/hu/tarsashazkezelo/tarsashaz/entity/DTO/ContractDTO.java
@@ -1,10 +1,18 @@
 package hu.tarsashazkezelo.tarsashaz.entity.DTO;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import hu.tarsashazkezelo.tarsashaz.entity.Role;
+import hu.tarsashazkezelo.tarsashaz.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Transient;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,5 +20,28 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ContractDTO {
 
-private Role role;
+    private Long id;
+    private String companyName;
+    private Integer taxNumber;
+    private String address;
+    private String name;
+    private String email;
+    private Integer phoneNumber;
+    private Role role;
+
+    public ContractDTO(User user) {
+        this.id = user.getId();
+        this.companyName = user.getCompanyName();
+        this.taxNumber = user.getTaxNumber();
+        this.address = user.getAddress();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.phoneNumber = user.getPhoneNumber();
+        this.role = user.getRole();
+    }
+    public User toEntity(){
+        User user = new User();
+        BeanUtils.copyProperties(this,user);
+        return user;
+    }
 }
--- thaz-management-backend/src/main/java/hu/tarsashazkezelo/tarsashaz/entity/DTO/RepresentativeDTO.java
+++ thaz-management-backend/src/main/java/hu/tarsashazkezelo/tarsashaz/entity/DTO/RepresentativeDTO.java
@@ -1,4 +1,36 @@
 package hu.tarsashazkezelo.tarsashaz.entity.DTO;
 
+import hu.tarsashazkezelo.tarsashaz.entity.Role;
+import hu.tarsashazkezelo.tarsashaz.entity.User;
+import org.springframework.beans.BeanUtils;
+
+
 public class RepresentativeDTO {
+    private Long id;
+    private String companyName;
+    private Integer taxNumber;
+    private String address;
+    private String name;
+    private String email;
+    private String password;
+    private Integer phoneNumber;
+    private Role role;
+
+    public RepresentativeDTO(User user) {
+        this.id = user.getId();
+        this.companyName = user.getCompanyName();
+        this.taxNumber = user.getTaxNumber();
+        this.address = user.getAddress();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.phoneNumber = user.getPhoneNumber();
+        this.role = user.getRole();
+    }
+
+    public User toEntity(){
+        User user = new User();
+        BeanUtils.copyProperties(this,user);
+        return user;
+    }
 }
